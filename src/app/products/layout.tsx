43bc8537365633cc46b6de62e04adb789fbab833
fadlystwn <fadlystwn@gmail.com>
2024-04-06 15:02:33 +0700

    Add @vercel/analytics dependency

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -1,10 +1,12 @@
 
+import { Analytics } from '@vercel/analytics/react';
 import styles from './layout.module.scss';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className={styles.layout}>
       <>
         {children}
+        <Analytics />
       </>
     </div>
   )
