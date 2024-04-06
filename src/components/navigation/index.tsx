e46bef9ab14b4f2fa8698b4a4e7e9a3959e1369d
fadlystwn <fadlystwn@gmail.com>
2024-04-06 08:09:14 +0700

    Add DesktopMenu and MobileMenu components to navigation

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,72 +1,8 @@
-import Link from 'next/link';
 import React, { useState } from 'react';
-import { Icon } from '@iconify/react';
+import DesktopMenu from '@/components/navigation/DesktopMenu';
+import MobileMenu from '@/components/navigation/MobileMenu';
 import { useAppDispatch, useAppSelector } from '@/stores/store';
 import { resetCartState } from '@/stores/cartSlice';
-import styles from './navigation.module.scss';
-
-interface DesktopMenuProps {
-  cartState: number;
-  handleCartReset: () => void;
-}
-
-const DesktopMenu: React.FC<DesktopMenuProps> = ({ cartState, handleCartReset }) => (
-  <div className={styles.navbar}>
-    <div className={styles.navbarLeft}>
-      <ul>
-        <li><Link href="/">Home</Link></li>
-        <li><a href="#">Care</a></li>
-        <li><a href="#">Sale</a></li>
-      </ul>
-    </div>
-    <div className={styles.navbarRight}>
-      <ul>
-        <li><a href="#">Sign Up</a></li>
-        <li><a href="#">Login</a></li>
-        <li className={styles.navButton}>
-          <button onClick={handleCartReset} className='button-secondary'>
-            <Icon className={styles.icon} icon="lets-icons:bag-alt-light" fontSize={18} /> Cart ({cartState})
-          </button>
-        </li>
-      </ul>
-    </div>
-  </div>
-);
-
-interface MobileMenuProps {
-  cartState: number; // Assuming cartState is a number
-  handleCartReset: () => void;
-  isOpen: boolean;
-  toggleMenu: () => void;
-}
-
-const MobileMenu: React.FC<MobileMenuProps> = ({ cartState, handleCartReset, isOpen, toggleMenu }) => (
-  <div className={styles.navbarMobile}>
-    <div className={styles.closeButton}>
-      <button className="button-secondary" onClick={toggleMenu}>
-        <Icon className={styles.icon} icon="bitcoin-icons:menu-outline" fontSize={18} />
-      </button>
-    </div>
-    {isOpen && (
-      <div className={styles.menu}>
-        <ul>
-          <li><Link href="/">Home</Link></li>
-          <li><a href="#">Care</a></li>
-          <li><a href="#">Sale</a></li>
-        </ul>
-        <ul>
-          <li><a href="#">Sign Up</a></li>
-          <li><a href="#">Login</a></li>
-          <li className={styles.navButton}>
-            <button className="button-secondary button-mobile" onClick={handleCartReset} >
-              <Icon className={styles.icon} icon="lets-icons:bag-alt-light" fontSize={18} /> Cart ({cartState})
-            </button>
-          </li>
-        </ul>
-      </div>
-    )}
-  </div>
-);
 
 const Navigation: React.FC = () => {
   const cartState = useAppSelector((state) => state.cart.cartState);
